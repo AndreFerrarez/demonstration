2067eb596c136bf4307f603806ed5e21a0fb64a1
Andre Ferrarez <andre.augusto.17@gmail.com>
2024-06-28 18:08:09 -0300

    bugFix Cors

--- demonstration/src/main/java/com/example/demonstration/Application.java
+++ demonstration/src/main/java/com/example/demonstration/Application.java
@@ -1,8 +1,12 @@
 package com.example.demonstration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default service URL")})
 @SpringBootApplication
 public class Application {
 
