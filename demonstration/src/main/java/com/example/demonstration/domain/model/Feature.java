88b0c886d9d110e65e8e5c3b9093455543793caa
Andre Ferrarez <andre.augusto.17@gmail.com>
2024-06-28 08:29:13 -0300

    Implementacao camada dominio

---
+++ demonstration/src/main/java/com/example/demonstration/domain/model/Feature.java
@@ -0,0 +1,8 @@
+package com.example.demonstration.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends Baseitem {
+
+}
---
+++ demonstration/src/main/java/com/example/demonstration/domain/model/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.demonstration.domain.model.Repository;
+
+import com.example.demonstration.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
