ac29ef8fc33fa884ce6c1ce686bb25ec03c1de31
Andre Ferrarez <andre.augusto.17@gmail.com>
2024-06-28 08:34:30 -0300

    Hotfix precision, scale

--- demonstration/src/main/java/com/example/demonstration/domain/model/Account.java
+++ demonstration/src/main/java/com/example/demonstration/domain/model/Account.java
@@ -14,9 +14,9 @@ public class Account {
     @Column(unique = true)
     private String number;
     private String agency;
-    @Column(nullable = false, scale = 13, precision = 2)
+    @Column(nullable = false, precision = 13, scale = 2)
     private BigDecimal balance;
-    @Column(name = "additional_limit", nullable = false, scale = 13, precision = 2)
+    @Column(name = "additional_limit", nullable = false, precision = 13, scale = 2)
     private BigDecimal limit;
 
     public Long getId() {
